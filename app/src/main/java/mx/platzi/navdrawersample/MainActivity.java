a168539ca16f2caaa5f9715bcb092bafa32badba
nRike <cAc0puTo>
2015-07-03 17:22:51 -0500

    Initial commit

---
+++ app/src/main/java/mx/platzi/navdrawersample/MainActivity.java
@@ -0,0 +1,106 @@
+package mx.platzi.navdrawersample;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private String drawerTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setToolbar(); // Setear Toolbar como action bar
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        drawerTitle = getResources().getString(R.string.home_item);
+        if (savedInstanceState == null) {
+            selectItem(drawerTitle);
+        }
+
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // Marcar item presionado
+                        menuItem.setChecked(true);
+                        // Crear nuevo fragmento
+                        String title = menuItem.getTitle().toString();
+                        selectItem(title);
+                        return true;
+                    }
+                }
+        );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void selectItem(String title) {
+        // Enviar título como arguemento del fragmento
+        Bundle args = new Bundle();
+        args.putString(mx.platzi.navdrawersample.PlaceholderFragment.ARG_SECTION_TITLE, title);
+        Fragment fragment = PlaceholderFragment.newInstance(title);
+        fragment.setArguments(args);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.main_content, fragment)
+                .commit();
+        drawerLayout.closeDrawers(); // Cerrar drawer
+        setTitle(title); // Setear título actual
+    }
+
+
+}
---
+++ app/src/main/java/mx/platzi/navdrawersample/PlaceholderFragment.java
@@ -0,0 +1,51 @@
+package mx.platzi.navdrawersample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import mx.platzi.navdrawersample.R;
+
+/**
+ * Fragment for the main content
+ */
+public class PlaceholderFragment extends Fragment {
+    /**
+     * This argument represents the title for each section
+     */
+    public static final String ARG_SECTION_TITLE = "section_number";
+
+
+    /**
+     * Crea una instancia prefabricada de {@link PlaceholderFragment}
+     *
+     * @param sectionTitle Título usado en el contenido
+     * @return Instancia dle fragmento
+     */
+    public static PlaceholderFragment newInstance(String sectionTitle) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_SECTION_TITLE, sectionTitle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.section_fragment, container, false);
+
+        // Ubicar argumento en el text view de section_fragment.xml
+        String title = getArguments().getString(ARG_SECTION_TITLE);
+        TextView titulo = (TextView) view.findViewById(R.id.title);
+        titulo.setText(title);
+        return view;
+    }
+
+}
